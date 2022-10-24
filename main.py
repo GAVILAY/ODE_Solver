b13449762e98fc71df4d879f452c938e16e8c660
Luis Armando GAVI <lagarviz.nubeformat@outlook.com>
2022-10-24 12:59:08 -0500

    Código ODE Solver extraido de la clase, sin cambios.

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop, Adam
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class ODEsolver(Sequential):
+    def _init_(self, **kwargs):
+        super()._init_(**kwargs)
+        self.loss_tracker = keras.metrics.Mean(name='loss')
 
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+    def train_step(self, data):
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        batch_size = tf.shape(data)[0]
+        x = tf.random.uniform((batch_size, 1), minval=-5, maxval=5)
 
+        with tf.GradientTape() as tape:
+            with tf.GradientTape() as tape2:
+                tape2.watch(x)
+                y_pred = self(x, training=True)
+            dy = tape2.gradient(y_pred, x)
+            x_0 = tf.zeros((batch_size, 1))
+            y_0 = self(x_0, training=True)
+            eq = x * dy + y_pred - x ** 2 * keras.backend.cos(x)
+            ic = y_0
+            loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0., ic)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+model = ODEsolver()
+
+model.add(Dense(10, activation='tanh', input_shape=(1,)))
+model.add(Dense(1, activation='tanh'))
+model.add(Dense(1, activation='linear'))
+
+model.summary()
+
+model.compile(optimizer=RMSprop(), metrics=['loss'])
+
+x = tf.linspace(-5, 5, 100)
+history = model.fit(x, epochs=500, verbose=1)
+
+x_testv = tf.linspace(-5, 5, 100)
+a = model.predict(x_testv)
+plt.plot(x_testv, a)
+#plt.plot(x_testv, np.exp(-x*x))
+plt.show()
+exit()
+
+model.save('red2.h5')
+modelo_cargado = tf.keras.models.load_model('red5.h5')
